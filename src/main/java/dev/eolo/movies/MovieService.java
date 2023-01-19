221212cf02b1adfee12fedeb30d34cf942d5e3f6
Éolo Charles <eolocharles@gmail.com>
2023-01-19 20:28:00 -0300

    API created

---
+++ src/main/java/dev/eolo/movies/MovieService.java
@@ -0,0 +1,25 @@
+package dev.eolo.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieByImdbId(String imdbId) {
+        return movieRepository.findByImdbId(imdbId);
+    }
+}
---
+++ src/test/java/dev/eolo/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eolo.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
